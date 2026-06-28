b8f401e4390635301010d8b208133bc0b34bfa17
ming <2528358258@qq.com>
2026-06-29 05:41:59 +0800

    feat(app): 添加移动端适配提示页面
    新增移动端设备检测逻辑，当在移动端打开时显示提示信息，引导用户在pc端打开项目

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <RouterView />
+  <div v-if="!isMobile">
+    <RouterView />
+  </div>
+  <div v-else style="text-align: center">
+    <div>没写自适应</div>
+    <div>请在pc端打开</div>
+  </div>
   <a class="a-link" href="https://github.com/OnZeng/chat-room-server" target="_blank">Github</a>
 </template>
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import { RouterView } from "vue-router";
 import { initSocketListeners } from "@/socket/socket";
+
+const isMobile = ref(false);
 
 onMounted(async () => {
   // 初始化监听器
   await initSocketListeners();
+  // 检查是否为移动端
+  const ua = navigator.userAgent.toLowerCase();
+  const Mobile = /android|iphone|ipod|ipad|windows phone|mobile/.test(ua);
+  if (Mobile) {
+    isMobile.value = true;
+  }
 });
 </script>
